862e951334fe5458f340379ea66c847c16a5facb
Samuel Gomez <contact@samuelgomez.fr>
2023-05-02 22:06:31 +0200

    feat(fix) : custom render

--- test/customRender.js
+++ test/customRender.js
@@ -2,9 +2,9 @@ import React from 'react'
 import {render as renderRTL} from '@testing-library/react'
 import PropTypes from 'prop-types'
 import {ThemeProvider} from 'emotion-theming'
-import {dark} from './../src/themes'
+import {dark} from '../src/themes'
 
-const render = (ui, {theme = dark, ...options}) => {
+const render = (ui, {theme = dark, ...options} = {}) => {
   const Wrapper = ({children}) => (
     <ThemeProvider theme={theme}>{children}</ThemeProvider>
   )
